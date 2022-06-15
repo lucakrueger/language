9075ae3cb80a4db932419d7ea793e665e8360c64
lucakrueger <lucakr@outlook.de>
2022-06-15 14:03:53 +0200

    Added Imports

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,25 @@ console.log(`Compiling ${argv[2]}`)
 var data = fs.readFileSync(argv[2], {encoding: 'utf-8'})
 try {
     var out = parser.parse(data)
+    var alreadyImported = new Map<String, Boolean>([])
+    var i = 0
+    var sze = out.uses.length
+    while(i < sze) {
+        var use = out.uses[i]
+        console.log(alreadyImported.get(use))
+        if(alreadyImported.get(use) != undefined) {
+            i++
+            continue
+        }
+
+        var importedData = fs.readFileSync(use, {encoding: 'utf-8'})
+        var imported = parser.parse(importedData)
+        out.src = imported.src + out.src
+        out.uses.push(...imported.uses)
+        sze = out.uses.length
+        alreadyImported.set(use, true)
+        i++
+    }
 } catch(e) {
     throw(e)
 }
--- todo.md
+++ todo.md
@@ -2,6 +2,4 @@
 
 - Variadic Functions
 - Operator Overloading
-- Fix Call Chains (Functions cannot be called, no argument size specified)
-- Importing
 - Better CLI
